c5f68d199288e4318ea696094a139663fdeea22e
Shubham singh <shubham1041singh@gmail.com>
2024-06-23 23:18:31 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,70 @@
+const questions = [
+    {
+        question:"who was the player of the tournament in 2019 world cup held in England?",
+        answers : [
+            {text : "pat cummins" , correct : false},
+            {text : "rohit sharma" , correct :  false},
+            {text : "kane willamson" , correct :  true},
+            {text : "ben stokes" , correct : false},
+            
+        ]
+    },
+    {
+        question:"who has taken the most wicktes in 2019 world cup held in england?",
+        answers : [
+            {text : "jasprit bumrah" , correct : false},
+            {text : "rashid khan" , correct : false},
+            {text : "mark wood" , correct : false},
+            {text : "mitchell starc" , correct : true},
+        ]
+    },
+    {
+        question:"who was the player of the match in 2023 world cup final?",
+        answers : [
+            {text : "jasprit bumrah" , correct : false},
+            {text : "travis head" , correct : true},
+            {text : "pat cummins" , correct : false},
+            {text : "mitchell starc" , correct : false},
+        ]
+    },
+    {
+        question:"who has won the player of the tournament in the 2023 world cup held in India?",
+        answers : [
+            {text : "jasprit bumrah" , correct : false},
+            {text : "rashid khan" , correct : false},
+            {text : "mark wood" , correct : false},
+            {text : "virat kohli" , correct : false},
+        ]
+    },
+    {
+        question:"what is name of the name of the trophy played between england and australia?",
+        answers : [
+            {text : "ashes" , correct : true},
+            {text : "border gavaskar trophy" , correct : false},
+            {text : "nelson mandella trophy" , correct : false},
+            {text : "euro cup" , correct : false},
+        ]
+    },
+
+]
+
+const questionElement = documnet.getElementById("question")
+const answerButtons = documnet.getElementById("answer-Buttons")
+const nextButton = document.getElementById("next-btn")
+
+let currentQuestionIndex=0
+let score=0
+
+function startQuiz(){
+    currentQuestionIndex=0
+     score=0
+     nextButton.innerHTML = "Next"
+     showQuestion()
+}
+
+function showQuestion()
+{
+  let currentQuestion   = questions[currentQuestionIndex]
+  let questionNo = currentQuestionIndex+1;
+  questionElement.innerHTML = q
+}
