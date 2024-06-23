11fd4585b85b30d9e7817f42a459b1314e58998b
Shubham singh <shubham1041singh@gmail.com>
2024-06-24 01:53:13 +0530

    ..........

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ const questions = [
             {text : "jasprit bumrah" , correct : false},
             {text : "rashid khan" , correct : false},
             {text : "mark wood" , correct : false},
-            {text : "virat kohli" , correct : false},
+            {text : "virat kohli" , correct : true},
         ]
     },
     {
@@ -48,8 +48,8 @@ const questions = [
 
 ]
 
-const questionElement = documnet.getElementById("question")
-const answerButton = documnet.getElementById("answer-buttons")
+const questionElement = document.getElementById("question")
+const answerButtons = document.getElementById("answer-buttons")
 const nextButton = document.getElementById("next-btn")
 
 let currentQuestionIndex=0
@@ -64,6 +64,7 @@ function startQuiz(){
 
 function showQuestion()
 {
+    resetState();
   let currentQuestion   = questions[currentQuestionIndex]
   let questionNo = currentQuestionIndex+1;
   questionElement.innerHTML = questionNo + ". " + currentQuestion.
@@ -74,8 +75,72 @@ function showQuestion()
     const button =  document.createElement("button")
     button.innerHTML = answer.text;
     button.classList.add("btn")
-    answerButton.appendChild(button);
+    answerButtons.appendChild(button);
+    if(answer.correct)
+        {
+            button.dataset.correct = answer.correct
+        }
+    button.addEventListener("click" , selectAnswer)
   })
 }
 
-startQuiz()
+function resetState()
+{
+     nextButton.style.display = "none"
+     while(answerButtons.firstChild)
+        {
+            answerButtons.removeChild(answerButtons.firstChild);
+        }
+}
+
+
+function selectAnswer(e)
+{
+   const selectedBtn = e.target;
+   const isCorrect = selectedBtn.dataset.correct==="true";
+   if(isCorrect)
+    {
+        selectedBtn.classList.add("correct")
+        score++;
+    }
+    else{
+        selectedBtn.classList.add("incorrect")
+    }
+    Array.from(answerButtons.children).forEach(button=>{
+        if(button.dataset.correct==="true")
+            {
+                button.classList.add('correct')
+            }
+            button.disabled = true;
+
+    });
+    nextButton.style.display = "block"
+}
+
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `you scored ${score} out of ${questions.length}`;
+    nextButton.innerHTML = "play again"
+    nextButton.style.display = "block"
+} 
+
+function handleNextButton(){
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length)
+        {
+            showQuestion()
+        }
+        else{
+             showScore();
+        }
+}
+
+nextButton.addEventListener("click" ,()=>{
+    if(currentQuestionIndex < questions.length)
+        {
+            handleNextButton();
+        }
+        else{
+            startQuiz()
+        }
+} )
